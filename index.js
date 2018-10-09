091a9a7a548f2f8df674d7c47b67817aa01a674a
ramadis <rramiro.o@hotmail.com>
2018-10-09 19:24:13 +0200

    New version to get data to create the 'delitos' file

--- index.js
+++ index.js
@@ -3,22 +3,48 @@ const fetch = require('node-fetch');
 const dotenv = require('dotenv');
 const omit = require('lodash.omit');
 const keys = require('lodash.keys');
-const months = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'];
+const _ = require('lodash');
+
+// Auxiliary functions
+function cartesian(...rest) {
+  return Array.prototype.reduce.call(rest, function(a, b) {
+    var ret = [];
+    a.forEach(function(a) {
+      b.forEach(function(b) {
+        ret.push(a.concat([b]));
+      });
+    });
+    return ret;
+  }, [[]]);
+}
 
 dotenv.config()
-
 const URI = process.env.URI;
+
+// Parameters to get all the data
+const params = {
+  years: [2016, 2017],
+  months: Array.from({ length: 12}).map((_, idx) => idx + 1),
+  crimes: Array.from({ length: 20 }).map((_, idx) => idx)
+}
+
+// Program
 
 const merge = () => {
   // Merge data
-  const merged = months.reduce((arr, month) => {
-    const file = JSON.parse(fs.readFileSync(`downloaded/${month}.json`, 'UTF-8'));
-    console.log(`✓ ${month} file read correctly`);
+
+  const files = fs.readdirSync('downloaded');
+
+  const merged = files.reduce((arr, fileName) => {
+    const file = JSON.parse(fs.readFileSync(`downloaded/${fileName}`, 'UTF-8'));
+    console.log(`✓ ${fileName} file read correctly`);
     return arr.concat(file);
   }, [])
 
+  const filtered = _.uniqWith(merged, (a, b) => a.id === b.id);
+
   // Save merged data
-  fs.writeFileSync('delitos.json', JSON.stringify(merged));
+  fs.writeFileSync('delitos.json', JSON.stringify(filtered));
   console.log(`✓ merged file created correctly`);
 }
 
@@ -31,24 +57,32 @@ const clean = (delito) => {
   delito.cantidad_vehiculos = Number(delito.cantidad_vehiculos);
 
   // Filter out unnecessary Ids
-  const toFilter = keys(delito).filter(key => key.endsWith('Id'));
-  return omit(delito, toFilter);
+  const unnecessaryKeys = keys(delito).filter(key => key.endsWith('Id'));
+  return omit(delito, unnecessaryKeys);
 }
 
 const download = async () => {
   if (!fs.existsSync('downloaded')) fs.mkdirSync('downloaded');
 
-  await Promise.all(months.map(async (month, number) => {
-    // Download data for a given month
-    const res = await fetch(URI.replace('MONTH', number + 1));
-    if (!res.ok) return console.warn(`${month} file could not be downloaded`);
+  const reqs = cartesian(params.years, params.months, params.crimes);
+
+  return Promise.all(reqs.map(async ([year, month, crime]) => {
+    const uri = URI.replace('MONTH', month)
+                   .replace('YEAR', year)
+                   .replace('CRIME', crime);
+    const id = `${year}-${month}|${crime}`;
+
+    const res = await fetch(uri);
+    if (!res.ok) return console.warn(`${id} file could not be downloaded`);
+
     const file = await res.json();
     const cleanFile = (file||[]).map(clean);
-    console.log(`✓ ${month} downloaded correctly`);
+    
+    console.log(`✓ ${id}  downloaded correctly`);
 
-    // Save data for a given month
-    fs.writeFileSync(`downloaded/${month}.json`, JSON.stringify(cleanFile));
-    console.log(`✓ ${month} file saved correctly`);
+    if (file.length < 1) return;
+    fs.writeFileSync(`downloaded/${id}.json`, JSON.stringify(cleanFile));
+    console.log(`✓ ${id} file saved correctly`);
   }));
 }
 
